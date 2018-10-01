8f0274ddb963271fccd34979fce1459bc2c2956a
rbanate <ricardo.banate@gmail.com>
2018-10-01 17:35:06 +0800

    added more icons

--- src/components/icons/icon.js
+++ src/components/icons/icon.js
@@ -3,8 +3,23 @@ import PropTypes from 'prop-types';
 
 import Close from './close';
 import Face from './face';
+import Dashboard from './dashboard';
+import DigixLogo from './digix-logo';
+import Magnifier from './magnifier';
+import Menu from './menu';
+import Notification from './notification';
+import Ribbon from './ribbon';
 
-const icons = { close: Close, face: Face };
+const icons = {
+  close: Close,
+  face: Face,
+  dasboard: Dashboard,
+  digixLogo: DigixLogo,
+  magnifier: Magnifier,
+  menu: Menu,
+  notification: Notification,
+  ribbon: Ribbon,
+};
 
 const Icon = props => {
   const IconComponent = icons[props.kind];
@@ -17,7 +32,7 @@ Icon.propTypes = {
 };
 
 Icon.defaultProps = {
-  kind: 'capsule',
+  kind: 'face',
 };
 
 export default Icon;
---
+++ src/components/icons/menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from './style';
+
+const MenuIcon = props => (
+  <Container {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+    </svg>
+  </Container>
+);
+
+MenuIcon.defaultProps = {
+  width: '1.6rem',
+  height: '1.6rem',
+};
+
+export default MenuIcon;

8c135734337ae9c58f48bc0fd087a6a05e68de63
Mireya Andres <mandres@digixglobal.com>
2019-01-14 12:26:58 +0800

    [FEATURE] Fetch data to show on the profile page. Ref: DGDG-30.
    The profile tab should be hidden when the user has not uploaded a wallet yet. Otherwise, clicking on the tab should show the user profile.
    
    This commit adds the `getDaoConfig` function that fetches data from the info-server. The `DEFAULT_MENU` items in `<CollapsibleMenu/>` also has an additional `public` attribute which controls what tabs are shown to users who have not uploaded a wallet yet.
    
    The following `data-digix` attributes were added for testing:
    - `Profile-Address`
    - `Profile-Status`
    - `Profile-QuarterPoints`
    - `Profile-ReputationPoints`
    - `Profile-Stake`
    - `Profile-ModerationRequirements` (div container element)
    - `Profile-ModerationRequirements-Reputation`
    - `Profile-ModerationRequirements-Stake`
    
    The following attributes were also added, but these still use placeholder data for now.
    - `Profile-QuarterParticipation`
    - `Profile-Proposals`
    - `Profile-DgxClaimed`
    - `Profile-KycStatus`

--- src/pages/user/profile/index.js
+++ src/pages/user/profile/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
 import { Button, Icon } from '@digix/gov-ui/components/common/elements/index';
+import { DEFAULT_STAKE_PER_DGD } from '@digix/gov-ui/constants';
+import { getDaoConfig, getDaoDetails } from '@digix/gov-ui/reducers/info-server/actions';
+import { truncateNumber } from '@digix/gov-ui/utils/helpers';
+
 import {
   ProfileWrapper,
   Heading,
@@ -23,36 +29,116 @@ import {
 } from '@digix/gov-ui/pages/user/profile/style';
 
 class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.STATUS = {
+      moderator: 'Moderator',
+      participant: 'Participant',
+      pastParticipant: 'Past Participant',
+      guest: 'Have not participated',
+    };
+  }
+
+  componentDidMount() {
+    const { getDaoConfigAction, getDaoDetailsAction } = this.props;
+    Promise.all([getDaoConfigAction(), getDaoDetailsAction()]);
+  }
+
+  getStakePerDgd = () => {
+    const { DaoDetails } = this.props;
+    const { startOfMainphase, startOfNextQuarter } = DaoDetails.data;
+
+    const currentTime = Date.now() / 1000;
+    const inLockingPhase = currentTime < startOfMainphase;
+    if (inLockingPhase) {
+      return DEFAULT_STAKE_PER_DGD;
+    }
+
+    return (startOfNextQuarter - currentTime) / (startOfNextQuarter - startOfMainphase);
+  };
+
+  getModeratorRequirements = () => {
+    const { AddressDetails, DaoConfig } = this.props;
+    const address = AddressDetails.data;
+    const config = DaoConfig.data;
+
+    const currentReputation = parseInt(address.reputationPoint, 10);
+    const minReputation = parseInt(config.CONFIG_MINIMUM_REPUTATION_FOR_MODERATOR, 10);
+    const requiredReputation = Math.max(0, minReputation - currentReputation);
+
+    const currentStake = parseInt(address.lockedDgdStake, 10);
+    const minStake = parseInt(config.CONFIG_MINIMUM_DGD_FOR_MODERATOR, 10);
+    const stakePerDgd = this.getStakePerDgd();
+    const requiredStake = stakePerDgd ? Math.max(0, (minStake - currentStake) / stakePerDgd) : 0;
+
+    const requirements = {
+      reputation: truncateNumber(requiredReputation),
+      stake: truncateNumber(requiredStake),
+    };
+
+    return requirements;
+  };
+
+  getStatus = () => {
+    const { AddressDetails } = this.props;
+    const address = AddressDetails.data;
+
+    if (address.isModerator) {
+      return this.STATUS.moderator;
+    } else if (address.isParticipant) {
+      return this.STATUS.participant;
+    } else if (address.lastParticipatedQuarter > 0) {
+      return this.STATUS.pastParticipant;
+    }
+
+    return this.STATUS.guest;
+  };
+
   render() {
+    const { AddressDetails } = this.props;
+    const address = AddressDetails.data;
+
+    const status = this.getStatus();
+    const stake = truncateNumber(address.lockedDgdStake);
+    const moderatorRequirements = this.getModeratorRequirements();
+    const hasUnmetModerationRequirements =
+      !address.isModerator &&
+      (moderatorRequirements.reputation > 0 || moderatorRequirements.stake > 0);
+
     return (
       <ProfileWrapper>
         <Heading>Profile</Heading>
         <UserInfo>
           <WalletInfo>
-            <span>User:</span>0x8827837920C8b78f4F4344796a911ace99e56263
+            <span>User:</span>
+            <span data-digix="Profile-Address">{address.address}</span>
           </WalletInfo>
           <UserStatus>
-            <span>Status:</span>Participant
+            <span>Status:</span>
+            <span data-digix="Profile-Status">{status}</span>
           </UserStatus>
         </UserInfo>
+
         <RewardSummary>
           <RewardItem column>
             <Label>Quarter Points</Label>
-            <Data>82</Data>
+            <Data data-digix="Profile-QuarterPoints">{address.quarterPoint}</Data>
           </RewardItem>
           <RewardItem column>
             <Label>Reputation Points</Label>
-            <Data>134</Data>
+            <Data data-digix="Profile-ReputationPoints">{address.reputationPoint}</Data>
           </RewardItem>
           <RewardItem column>
             <Label>My Stake</Label>
-            <Data>65.75</Data>
+            <Data data-digix="Profile-Stake">{stake}</Data>
           </RewardItem>
         </RewardSummary>
+
+        {/* TODO: Fetch data for this section */}
         <ActivitySummary>
           <ActivityItem>
             <Label>Participated In</Label>
-            <Data>12</Data>
+            <Data data-digix="Profile-QuarterParticipation">12</Data>
             <Label>Quarter(s)</Label>
             <Actions>
               <Button primary>More Info</Button>
@@ -60,7 +146,7 @@ class Profile extends React.Component {
           </ActivityItem>
           <ActivityItem>
             <Label>Proposed</Label>
-            <Data>0</Data>
+            <Data data-digix="Profile-Proposals">0</Data>
             <Label>Project(s)</Label>
             <Actions>
               <Button primary>More Info</Button>
@@ -68,7 +154,7 @@ class Profile extends React.Component {
           </ActivityItem>
           <ActivityItem>
             <Label>Claimed</Label>
-            <Data>12</Data>
+            <Data data-digix="Profile-DgxClaimed">12</Data>
             <Label>DGX</Label>
             <Actions>
               <Button primary>More Info</Button>
@@ -76,36 +162,70 @@ class Profile extends React.Component {
           </ActivityItem>
           <ActivityItem>
             <Label>KYC Status</Label>
-            <Data>Not Verified</Data>
+            <Data data-digix="Profile-KycStatus">Not Verified</Data>
             <Label>&nbsp;</Label>
             <Actions>
               <Button primary>Submit KYC</Button>
             </Actions>
           </ActivityItem>
         </ActivitySummary>
-        <Moderation>
-          <Label>TO GAIN MODERATOR STATUS, YOU WILL NEED ANOTHER</Label>
-          <Criteria>
-            <ModeratorReputation>
-              <Data>9866</Data>
-              <ModeratorLabel>Reputation Points</ModeratorLabel>
-            </ModeratorReputation>
-            <Plus>
-              <Icon kind="plus" />
-            </Plus>
-            <ModeratorStake>
-              <Data>934.25</Data>
-              <ModeratorLabel>Stake</ModeratorLabel>
-            </ModeratorStake>
-          </Criteria>
-          <Actions>
-            <Button primary>Redeem Badge</Button>
-            <Button primary>Lock More DGD</Button>
-          </Actions>
-        </Moderation>
+
+        {hasUnmetModerationRequirements && (
+          <Moderation data-digix="Profile-ModerationRequirements">
+            <Label>TO GAIN MODERATOR STATUS, YOU WILL NEED ANOTHER</Label>
+            <Criteria>
+              <ModeratorReputation>
+                <Data data-digix="Profile-ModerationRequirements-Reputation">
+                  {moderatorRequirements.reputation}
+                </Data>
+                <ModeratorLabel>Reputation Points</ModeratorLabel>
+              </ModeratorReputation>
+              <Plus>
+                <Icon kind="plus" />
+              </Plus>
+              <ModeratorStake>
+                <Data data-digix="Profile-ModerationRequirements-Stake">
+                  {moderatorRequirements.stake}
+                </Data>
+                <ModeratorLabel>Stake</ModeratorLabel>
+              </ModeratorStake>
+            </Criteria>
+            <Actions>
+              <Button primary>Redeem Badge</Button>
+              <Button primary>Lock More DGD</Button>
+            </Actions>
+          </Moderation>
+        )}
       </ProfileWrapper>
     );
   }
 }
 
-export default Profile;
+const { func, object } = PropTypes;
+
+Profile.propTypes = {
+  AddressDetails: object.isRequired,
+  DaoConfig: object,
+  DaoDetails: object,
+  getDaoConfigAction: func.isRequired,
+  getDaoDetailsAction: func.isRequired,
+};
+
+Profile.defaultProps = {
+  DaoConfig: undefined,
+  DaoDetails: undefined,
+};
+
+const mapStateToProps = ({ infoServer }) => ({
+  AddressDetails: infoServer.AddressDetails,
+  DaoConfig: infoServer.DaoConfig,
+  DaoDetails: infoServer.DaoDetails,
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    getDaoConfigAction: getDaoConfig,
+    getDaoDetailsAction: getDaoDetails,
+  }
+)(Profile);
--- src/reducers/info-server/actions.js
+++ src/reducers/info-server/actions.js
@@ -1,6 +1,7 @@
 import { REDUX_PREFIX, INFO_SERVER } from './constants';
 
 export const actions = {
+  GET_DAO_CONFIG: `${REDUX_PREFIX}GET_DAO_CONFIG`,
   GET_DAO_DETAILS: `${REDUX_PREFIX}GET_DAO_DETAILS`,
   GET_ADDRESS_DETAILS: `${REDUX_PREFIX}GET_ADDRESS_DETAILS`,
   GET_PROPOSALS: `${REDUX_PREFIX}GET_PROPOSALS`,
@@ -38,6 +39,10 @@ function fetchData(url, type) {
       })
       .catch(error => dispatch({ type, payload: { fetching: false, error } }));
   };
+}
+
+export function getDaoConfig() {
+  return fetchData(`${INFO_SERVER}/daoConfigs`, actions.GET_DAO_CONFIG);
 }
 
 export function getDaoDetails() {

cba456a898004c8d9939d6aec7dc3eb6104f2a7f
Mireya Andres <mandres@digixglobal.com>
2019-01-23 12:30:11 +0800

    [FEATURE] Update email from profile page.
    Ref: DGDG-251.
    
    User should be able to update their email any time from the profile page. This implements the `changeEmail` mutation to update the email via graphql.

--- src/components/common/blocks/overlay/profile-email/index.js
+++ src/components/common/blocks/overlay/profile-email/index.js
@@ -1,42 +1,116 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import UpdateEmailButton from '@digix/gov-ui/components/common/blocks/overlay/profile-email/update-email';
 import web3Connect from 'spectrum-lightsuite/src/helpers/web3/connect';
-import { Button, Input } from '@digix/gov-ui/components/common/elements/index';
+import { CallToAction } from '@digix/gov-ui/components/common/blocks/overlay/profile-email/style';
+import { Input } from '@digix/gov-ui/components/common/elements/index';
 import {
   IntroContainer,
   OverlayHeader as Header,
   Label,
   Hint,
 } from '@digix/gov-ui/components/common/common-styles';
-
-import { CallToAction } from '@digix/gov-ui/components/common/blocks/overlay/profile-email/style';
+import { showHideAlert, showRightPanel } from '@digix/gov-ui/reducers/gov-ui/actions';
 
 class EmailOverlay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      error: undefined,
+    };
+
+    // eslint-disable-next-line
+    this.EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    this.ERROR_MESSAGES = {
+      invalid: `Please enter a valid email address.`,
+      unchanged: `Please enter an email address that is different from your current email.`,
+      connectionError: `Unable to update email. Please try again.`,
+    };
+  }
+
+  onEmailUpdate = response => {
+    const { errors } = response.changeEmail;
+    if (errors.length) {
+      this.setState({ error: errors[0].message });
+      return;
+    }
+
+    this.props.showRightPanel({ show: false });
+    this.props.showHideAlert({
+      message: 'Email updated',
+    });
+  };
+
+  onEmailUpdateError = error => {
+    this.setState({
+      error: this.ERROR_MESSAGES.connectionError,
+    });
+  };
+
+  handleInput = e => {
+    const email = e.target.value.trim();
+    const { currentEmail } = this.props;
+    let error;
+
+    if (!this.EMAIL_REGEX.test(email)) {
+      error = this.ERROR_MESSAGES.invalid;
+    } else if (currentEmail && email === currentEmail) {
+      error = this.ERROR_MESSAGES.unchanged;
+    }
+
+    this.setState({ email, error });
+  };
+
+  renderHint = () => {
+    const { error } = this.state;
+    if (!error) {
+      return null;
+    }
+
+    return <Hint error>{error}</Hint>;
+  };
+
   render() {
+    const { email, error } = this.state;
+    const disableButton = !email || email === '' || !!error;
+
     return (
       <IntroContainer>
         <Header uppercase>Set Email</Header>
         <Label>Please enter the email you want to link to your address</Label>
-        <Input type="text" data-digix="SetEmail-Textbox" />
-        <Hint>
-          This email is already used with a DigixDAO account. Please try another email address.
-        </Hint>
+        <Input type="text" data-digix="SetEmail-Textbox" onChange={this.handleInput} />
+        {this.renderHint()}
         <CallToAction>
-          <Button primary fluid large data-digix="SetEmail-Cta">
-            Set Email
-          </Button>
+          <UpdateEmailButton
+            disable={disableButton}
+            email={email}
+            onEmailUpdate={this.onEmailUpdate}
+            onEmailUpdateError={this.onEmailUpdateError}
+          />
         </CallToAction>
       </IntroContainer>
     );
   }
 }
 
-EmailOverlay.propTypes = {};
+const { func, string } = PropTypes;
+EmailOverlay.propTypes = {
+  currentEmail: string.isRequired,
+  showHideAlert: func.isRequired,
+  showRightPanel: func.isRequired,
+};
+
 const mapStateToProps = () => ({});
 export default web3Connect(
   connect(
     mapStateToProps,
-    {}
+    {
+      showHideAlert,
+      showRightPanel,
+    }
   )(EmailOverlay)
 );

c2f1c15cf290d176f2e07348b0e13f19588ed283
Vivian Guzman <vlopez@digixglobal.com>
2019-02-07 09:49:42 +0800

    Resize down the width of side bar

--- src/hocs/style.js
+++ src/hocs/style.js
@@ -9,7 +9,7 @@ export const Container = styled.div`
 export const ContentWrapper = styled.div`
   background: ${props => props.theme.backgroundTertiary.lightest.toString()};
   min-height: 100vh;
-  padding: 12em;
+  padding: 12em 8rem;
 
   ${media.tablet`padding: 12rem 8rem;`}
   ${media.mobile`padding: 12rem 3rem;`}

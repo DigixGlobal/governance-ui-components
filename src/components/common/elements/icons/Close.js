7a167cd5875b15566d3fe3a7f0838b74b3730372
Vivian Guzman <vlopez@digixglobal.com>
2019-02-19 14:35:59 +0800

    Create Claim Approval Overlay

--- src/components/common/elements/icons/Close.js
+++ src/components/common/elements/icons/Close.js
@@ -4,14 +4,12 @@ import { Container } from './style';
 const CloseIcon = props => (
   <Container {...props}>
     <svg viewBox="0 0 13.91 13.91">
-      <a xlinkHref="#" target="_blank" cursor="pointer" pointerEvents="all">
-        <path
-          className="cls-1"
-          d="M8.35,7l5.56,5.57-1.39,1.39L7,8.35,1.39,13.91,0,12.52,5.56,7,0,1.39,1.39,0,7,5.56,12.52,0l1.39,1.39Z"
-          transform="translate(0 0)"
-        />
-        <rect x="0" y="0" width="100%" height="100%" fill="none" />
-      </a>
+      <path
+        className="cls-1"
+        d="M8.35,7l5.56,5.57-1.39,1.39L7,8.35,1.39,13.91,0,12.52,5.56,7,0,1.39,1.39,0,7,5.56,12.52,0l1.39,1.39Z"
+        transform="translate(0 0)"
+      />
+      <rect x="0" y="0" width="100%" height="100%" fill="none" />
     </svg>
   </Container>
 );

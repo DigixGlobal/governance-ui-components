24ab304b6e78eb89fc4ea2daad663fcb48af61fd
veelab8 <37611942+veelab8@users.noreply.github.com>
2019-02-20 14:14:12 +0800

    Create error message overlay (#85)

--- src/components/common/elements/icons/Close.js
+++ src/components/common/elements/icons/Close.js
@@ -9,7 +9,6 @@ const CloseIcon = props => (
         d="M8.35,7l5.56,5.57-1.39,1.39L7,8.35,1.39,13.91,0,12.52,5.56,7,0,1.39,1.39,0,7,5.56,12.52,0l1.39,1.39Z"
         transform="translate(0 0)"
       />
-      <rect x="0" y="0" width="100%" height="100%" fill="none" />
     </svg>
   </Container>
 );

c5cf7d5eedf5efd8e6c2a221b26aa99e0638af1a
rbanate <ricardo.banate@gmail.com>
2019-02-27 12:46:37 +0800

    added locked dgd on Profile and Dashboard

--- src/pages/user/profile/index.js
+++ src/pages/user/profile/index.js
@@ -291,7 +291,7 @@ class Profile extends React.Component {
             <Label>My Stake</Label>
             <Data data-digix="Profile-Stake">
               <span>{stake}</span>
-              <span className="equiv">(0 DGD LOCKED)</span>
+              <span className="equiv">({AddressDetails.data.lockedDgd} DGD LOCKED)</span>
             </Data>
           </Item>
         </UserStats>

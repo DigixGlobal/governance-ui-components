c1eab827f04598dad427a442279b6bcabcae1b58
Ricardo Banate <ricardo.banate@gmail.com>
2019-03-21 11:06:25 +0800

    More proper handling of uploading images when editing projects (#156)
    * handling uploading of images
    
    * Updated Create Proposal and Edit Proposal to allow deleting of uploaded images

--- src/pages/proposals/forms/multimedia.js
+++ src/pages/proposals/forms/multimedia.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Modal from 'react-responsive-modal';
@@ -6,7 +6,6 @@ import Modal from 'react-responsive-modal';
 import Button from '@digix/gov-ui/components/common/elements/buttons/index';
 
 import { dijixImageConfig, dijixPdfConfig, dijix } from '@digix/gov-ui/utils/dijix';
-import ImageViewer from '@digix/gov-ui/components/common/ipfs-viewer';
 
 import {
   Fieldset,
@@ -16,16 +15,52 @@ import {
   ImageHolder,
 } from '@digix/gov-ui/pages/proposals/forms/style';
 
+import { fetchImages } from '@digix/gov-ui/pages/proposals/image-helper';
+
 class Multimedia extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      open: false,
+      files: undefined,
       thumbnails: undefined,
+      selectedImage: undefined,
     };
   }
 
-  showHideImage = () => {
-    this.setState({ open: !this.state.open });
+  componentDidMount = () => {
+    const { images } = this.props.form;
+    if (images) fetchImages(images).then(files => this.setState({ files }));
+  };
+
+  showHideImage = source => () => {
+    this.setState({ open: !this.state.open, selectedImage: source });
+  };
+
+  handleDeleteNewlyUploaded = index => () => {
+    const { thumbnails } = this.state;
+    const { proofs } = this.props.form;
+    if (index < 0 || (!thumbnails && !proofs)) return;
+
+    if (thumbnails) thumbnails.splice(index, 1);
+
+    if (proofs) proofs.splice(index, 1);
+
+    this.setState({ thumbnails: thumbnails || proofs }, () => {
+      this.props.onChange('proofs', proofs);
+    });
+  };
+
+  handleDeleteExisting = index => () => {
+    const { images } = this.props.form;
+    const { files } = this.state;
+    if (index < 0 || !images) return;
+    files.splice(index, 1);
+    const filtered = images.filter((img, i) => i !== index);
+
+    this.setState({ files }, () => {
+      this.props.onChange('images', filtered);
+    });
   };
 
   handleUpload = e => {
@@ -57,11 +92,17 @@ class Multimedia extends React.Component {
           }
 
           if (file.type === 'image/png' || file.type === 'image/jpeg') {
-            thumbs.push({ fileType: file.type, src: result, name: file.name });
+            thumbs.push({
+              fileType: file.type,
+              src: result,
+              name: file.name,
+              index: thumbs.length > 0 ? thumbs.length - 1 : 0,
+            });
 
             proofsArray.push({
               type: 'image',
               src: result.toString(),
+              index: proofsArray.length > 0 ? proofsArray.length - 1 : 0,
               embed: 'imageKey',
               ...dijixImageConfig,
             });
@@ -72,6 +113,7 @@ class Multimedia extends React.Component {
             proofsArray.push({
               type: 'pdf',
               src: file,
+              index: proofsArray.length > 0 ? proofsArray.length - 1 : 0,
               embed: 'pdfKey',
               ...dijixPdfConfig,
             });
@@ -87,48 +129,40 @@ class Multimedia extends React.Component {
     }
   };
 
-  renderDocuments(documents) {
+  renderDocuments(documents, existing) {
     if (!documents) return null;
 
-    return this.renderImages(documents);
+    return this.renderImages(documents, existing);
   }
 
-  renderImages = (proofs, preview) => {
-    if (!proofs) return null;
-    const images = proofs.map((img, i) => (
-      <Fragment key={`img-${i + 1}`}>
-        {/* eslint-disable */}
-        <img
-          key={`img-${i + 1}`}
-          alt=""
-          onClick={this.showHideImage}
-          src={
-            preview
-              ? img.thumbnail
-              : `${dijix.config.httpEndpoint}/${img.thumbnail}?q=${Date.now()}`
-          }
-        />
-        <Modal open={this.state.open} onClose={this.showHideImage}>
-          <div>
-            <img
-              key={`img-${i + 1}`}
-              alt=""
-              style={{width:'100%'}}
-              src={preview ? img.src : `${dijix.config.httpEndpoint}/${img.src}?q=${Date.now()}`}
-              />
-            <Button kind="round" small onClick={this.showHideImage}>
-              Close
-            </Button>
-          </div>
-        </Modal>
-        {/* eslint-enable */}
-      </Fragment>
-    ));
+  renderImages = (proofs, existing) => {
+    if (!proofs || proofs.length === 0) return null;
+    const images = proofs.map((img, i) => {
+      let source = img.thumbnail;
+
+      if (!source && img.src) {
+        source = img.src;
+      } else if (source) {
+        source = `${dijix.config.httpEndpoint}/${img.src}`;
+      }
+      return (
+        <div key={`img-${i + 1}`}>
+          <Button
+            onClick={existing ? this.handleDeleteExisting(i) : this.handleDeleteNewlyUploaded(i)}
+          >
+            Delete
+          </Button>
+          {/* eslint-disable*/}
+          <img  alt="" onClick={this.showHideImage(source)} src={source} />;
+          {/* eslint-enable */}
+        </div>
+      );
+    });
     return images;
   };
 
   render() {
-    const { thumbnails } = this.state;
+    const { thumbnails, files, selectedImage } = this.state;
     const { proofs, images: imageHash } = this.props.form;
     const images = thumbnails || proofs;
     return (
@@ -156,20 +190,20 @@ class Multimedia extends React.Component {
             </div>
             <div>
               <ImageHolder>
-                {images &&
-                  images.map(image => <img key={image.name} alt={image.name} src={image.src} />)}
-                {imageHash && (
-                  <ImageViewer
-                    thumbnailSize="512"
-                    hashes={imageHash}
-                    renderLoading={null}
-                    renderResolved={thumbs => this.renderDocuments(thumbs)}
-                  />
-                )}
+                {images && this.renderDocuments(images)}
+                {imageHash && this.renderDocuments(files, true)}
               </ImageHolder>
             </div>
           </MediaUploader>
         </FormItem>
+        <Modal open={this.state.open} onClose={this.showHideImage()}>
+          <div>
+            <img alt="" style={{ width: '100%' }} src={selectedImage} />
+            <Button kind="round" onClick={this.showHideImage()}>
+              Close
+            </Button>
+          </div>
+        </Modal>
       </Fieldset>
     );
   }

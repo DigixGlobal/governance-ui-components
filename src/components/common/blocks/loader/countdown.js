1cdf5a6ca11225c9f1d12b17ed3f82eb04575c75
Vivian Guzman <vlopez@digixglobal.com>
2019-03-29 08:31:33 +0800

    Remove unused import link

--- src/components/common/blocks/loader/countdown.js
+++ src/components/common/blocks/loader/countdown.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import Countdown from 'react-countdown-now';
 import ScreenLoader from '@digix/gov-ui/components/common/blocks/loader/screen';

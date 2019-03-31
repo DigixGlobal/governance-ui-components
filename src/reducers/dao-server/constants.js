ec3c1fdf8490a88c431674915626699408e051f4
mrenoon <vunguyenvyvp@gmail.com>
2019-03-31 12:07:17 +0800

    merge latest changes from develop (sprint-7a)

--- src/reducers/dao-server/constants.js
+++ src/reducers/dao-server/constants.js
@@ -1,6 +1,6 @@
 export const REDUX_PREFIX = 'DAO_SERVER_';
 
 export const DAO_SERVER =
-  (process.env.ENVIRONMENT === 'production' && 'https://dao.digix.global') ||
+  (process.env.ENVIRONMENT === 'production' && 'https://daoapi.digix.global') ||
   (process.env.ENVIRONMENT === 'kovan' && 'https://dao.digixdev.com') ||
   'http://localhost:3005';

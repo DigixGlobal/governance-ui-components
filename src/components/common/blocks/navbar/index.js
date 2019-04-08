48b10f041ea69a3bdc7a5398d8fb1dd8ca1640b0
Ricardo Banate <ricardo.banate@gmail.com>
2019-04-08 14:34:05 +0800

    added back utility compnent to fix popup issue (#225)

--- src/components/common/blocks/navbar/index.js
+++ src/components/common/blocks/navbar/index.js
@@ -4,6 +4,7 @@ import Search from '@digix/gov-ui/components/common/blocks/navbar/search';
 import WalletButton from '@digix/gov-ui/components/common/blocks/navbar/wallet';
 import { Header, Digix, NavItem } from '@digix/gov-ui/components/common/blocks/navbar/style';
 import Translator from '@digix/gov-ui/components/common/blocks/navbar/translator';
+import Utility from '@digix/gov-ui/components/common/blocks/navbar/utility';
 
 class NavBar extends React.Component {
   render() {
@@ -15,6 +16,7 @@ class NavBar extends React.Component {
         <NavItem>
           <Digix kind="logo" />
         </NavItem>
+        <Utility />
       </Header>
     );
   }

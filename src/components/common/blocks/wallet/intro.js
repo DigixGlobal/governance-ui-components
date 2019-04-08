9f578c7810043de7ca78260bf916594e6237eac0
Mireya Andres <mandres@digixglobal.com>
2019-04-08 11:02:18 +0800

    [TRANSLATIONS] Translate Load Wallet
    Ref: [DGDG-482](https://tracker.digixdev.com/issue/DGDG-482)
    
    As titled. This also adds missing translations for the Approve Interaction overlay.

--- src/components/common/blocks/wallet/intro.js
+++ src/components/common/blocks/wallet/intro.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import Button from '@digix/gov-ui/components/common/elements/buttons/';
 import Icon from '@digix/gov-ui/components/common/elements/icons';
 import {
   CloseButton,
   IntroContainer,
-  Link,
   OverlayHeader as Header,
 } from '@digix/gov-ui/components/common/common-styles';
 import { ActionContainer } from '@digix/gov-ui/components/common/blocks/wallet/style.js';
@@ -22,19 +22,18 @@ class Intro extends React.Component {
 
   render() {
     const { onClose } = this.props;
+    const t = this.props.translations.loadWallet.introOverlay;
+
     return (
       <IntroContainer>
         <CloseButton>
           <Icon kind="close" onClick={onClose} />
         </CloseButton>
-        <Header>Hello there!</Header>
-        <p>
-          You will need to load a wallet and lock some DGD in before you can browse projects and
-          vote in DigixDAO
-        </p>
+        <Header>{t.title}</Header>
+        <p>{t.instructions}</p>
         <ActionContainer>
           <Button kind="round" secondary fluid large onClick={this.handleButtonClick}>
-            Load Wallet
+            {t.button}
           </Button>
         </ActionContainer>
       </IntroContainer>
@@ -42,10 +41,29 @@ class Intro extends React.Component {
   }
 }
 
-const { func } = PropTypes;
+const { func, object } = PropTypes;
 
 Intro.propTypes = {
   onChangeStage: func.isRequired,
   onClose: func.isRequired,
+  translations: object,
 };
-export default Intro;
+
+Intro.defaultProps = {
+  translations: {
+    loadWallet: {
+      introOverlay: {
+        title: 'Hello there!',
+        instructions:
+          'You will need to load a wallet and lock some DGD in before you can browse projects and vote in DigixDAO.',
+        button: 'Load Wallet',
+      },
+    },
+  },
+};
+
+const mapStateToProps = state => ({
+  translations: state.daoServer.Translations.data,
+});
+
+export default connect(mapStateToProps)(Intro);

a428ff1a66aadd3c13eb81dd3e4573da719994bd
rbanate <ricardo.banate@gmail.com>
2019-05-14 10:55:08 +0800

    hide special proposal details

--- src/pages/proposals/special-project-details.js
+++ src/pages/proposals/special-project-details.js
@@ -6,7 +6,13 @@ import { DetailsContainer, Content, SubTitle } from './style';
 const SpecialProjectDetails = props => (
   <DetailsContainer>
     <Content>
-      <SubTitle>Configuration Details</SubTitle>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      {/* TODO: re-instate this later once special proposals need to be displayed */}
+      {/* <SubTitle>Configuration Details</SubTitle>
       {Object.keys(props.uintConfigs).map(key => {
         if (key === '__typename') return null;
         return (
@@ -17,12 +23,12 @@ const SpecialProjectDetails = props => (
             <span>{props.uintConfigs[key]}</span>
           </div>
         );
-      })}
+      })} */}
     </Content>
   </DetailsContainer>
 );
 
 SpecialProjectDetails.propTypes = {
-  uintConfigs: PropTypes.object.isRequired,
+  // uintConfigs: PropTypes.object.isRequired,
 };
 export default SpecialProjectDetails;

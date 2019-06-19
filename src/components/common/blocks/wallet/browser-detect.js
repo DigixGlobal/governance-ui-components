fb11caa68292e25efb1f833fba477699d83983f3
Muhammad Daivasmara Denaw <Daivasmara@gmail.com>
2019-06-19 12:23:04 +0700

    remove the blocklist variable

--- src/components/common/blocks/wallet/browser-detect.js
+++ src/components/common/blocks/wallet/browser-detect.js
@@ -5,22 +5,18 @@
 export function browserDetection() {
   const isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
   const isFirefox = typeof InstallTrigger !== 'undefined';
-  const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
   const isSafari = /constructor/i.test(window.HTMLElement) || (function(p) { return p.toString() === '[object SafariRemoteNotification]';})(!window.safari || (typeof safari !== 'undefined' && safari.pushNotification));
-
-    let blocklist = [];
+  const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
     if (isOpera) {
-        blocklist = ['TREZOR', 'METAMASK'];
-        return blocklist;
+        return ['TREZOR', 'METAMASK'];
     } else if (isFirefox) {
-        blocklist = ['LEDGER'];
+        return ['LEDGER'];
     } else if (isSafari) {
-        blocklist = ['TREZOR', 'METAMASK', 'LEDGER'];
+        return ['TREZOR', 'METAMASK', 'LEDGER'];
     } else if (isChrome) {
-        blocklist = [];
-        return blocklist;
+        return [];
     }
 
-    return blocklist;
+    return [];
 }

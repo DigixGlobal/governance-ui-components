affc721f27ff209ec5c4e56427820f97b9035e98
Muhammad Daivasmara Denaw <Daivasmara@gmail.com>
2019-06-20 10:30:18 +0700

    [ENH] Disabled wallet buttons if the browser does not support it (#348)
    * added a browser detections and created an easy-to-manage blocklist for the wallets
    
    * disable the wallet if it's on the blocklist
    
    * remove the blocklist variable
    
    * removed console log for the browser
    
    * fixed the issue where the button is disabled but the modal is still popping when clicked

--- src/components/common/blocks/wallet/trezor/index.js
+++ src/components/common/blocks/wallet/trezor/index.js
@@ -11,7 +11,28 @@ import Icon from '@digix/gov-ui/components/common/elements/icons';
 import { LogLoadWallet } from '@digix/gov-ui/analytics/loadWallet';
 
 class Trezor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blocked: false,
+    };
+  }
+
+  componentDidMount = () => {
+    this.getBlockList();
+  };
+
+  getBlockList = () => {
+    const { blockList } = this.props;
+    if (blockList.includes('TREZOR')) {
+      this.setState({
+        blocked: true,
+      });
+    }
+  };
+
   render() {
+    const { blocked } = this.state;
     return (
       (
         <DefaultAddressSelector
@@ -25,40 +46,43 @@ class Trezor extends React.Component {
         />
       ),
       (
-        <KeystoreModal
-          createKeystore={this.props.createKeystore}
-          onSuccess={() => this.props.onSuccess()}
-          showBalances
-          key="trezor-popup"
-          submitFunc={this.props.createKeystore}
-          form={KeystoreCreationForm}
-          creatingKeyStore
-          data={{ type: 'trezor', updateDefaultAddress: true }}
-          header="Load Trezor Wallet"
-          hideSelector
-          allowedKeystoreTypes={['trezor']}
-          translations={this.props.translations}
-          commonTranslations={this.props.commonTranslations}
-          logLoadWallet={LogLoadWallet}
-          trigger={
-            <Button kind="round" secondary fluid showIcon large>
-              <Icon kind="trezor" />
-              Trezor
-            </Button>
-          }
-        />
+        <div style={blocked ? { pointerEvents: 'none' } : null}>
+          <KeystoreModal
+            createKeystore={this.props.createKeystore}
+            onSuccess={() => this.props.onSuccess()}
+            showBalances
+            key="trezor-popup"
+            submitFunc={this.props.createKeystore}
+            form={KeystoreCreationForm}
+            creatingKeyStore
+            data={{ type: 'trezor', updateDefaultAddress: true }}
+            header="Load Trezor Wallet"
+            hideSelector
+            allowedKeystoreTypes={['trezor']}
+            translations={this.props.translations}
+            commonTranslations={this.props.commonTranslations}
+            logLoadWallet={LogLoadWallet}
+            trigger={
+              <Button kind="round" secondary fluid showIcon large disabled={blocked}>
+                <Icon kind="trezor" />
+                Trezor
+              </Button>
+            }
+          />
+        </div>
       )
     );
   }
 }
 
-const { func, object } = PropTypes;
+const { func, object, array } = PropTypes;
 
 Trezor.propTypes = {
   commonTranslations: object.isRequired,
   createKeystore: func,
   onSuccess: func.isRequired,
   translations: object.isRequired,
+  blockList: array.isRequired,
 };
 
 Trezor.defaultProps = {

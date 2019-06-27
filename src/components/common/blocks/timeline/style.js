711856255bb6e414594506116804bd5b0d47e083
Muhammad Daivasmara Denaw <Daivasmara@gmail.com>
2019-06-27 17:08:13 +0700

    added new UI for stakes and rewards

--- src/components/common/blocks/timeline/style.js
+++ src/components/common/blocks/timeline/style.js
@@ -32,6 +32,35 @@ export const Qtr = styled.div`
   display: flex;
   align-items: left;
   margin-bottom: 5rem;
+  ${media.mobile`
+    margin-bottom: 2.5rem;
+  `};
+`;
+
+export const Toggle = styled.a`
+  color: ${props => props.theme.textColor.secondary.base.toString()};
+  font-family: 'Futura PT Book', sans-serif;
+  font-size: 1.4em;
+  text-transform: uppercase;
+  position: absolute;
+  right: 5rem;
+  top: 6rem;
+  > div {
+    width: 1.25rem;
+    height: 1.25rem;
+    margin-left: 1rem;
+
+    svg {
+      transform: rotate(-90deg);
+    }
+    
+  ${media.mobile`
+    position: relative;
+    text-align: right;
+    right: 0;
+    top: 0;
+    margin-bottom: 2.5rem;
+  `};
 `;
 
 export const Stats = styled(Card)`
